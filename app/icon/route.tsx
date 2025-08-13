2378247aaa1bc4d8db0ce98cbee05e712165ad33
aswinzz <aswinvb.aswin6@gmail.com>
2025-08-13 09:03:18 +0530

    fix: icons

--- app/icon/route.tsx
+++ app/icon/route.tsx
@@ -2,8 +2,6 @@ import { ImageResponse } from 'next/og'
 
 export const runtime = 'edge'
 
-export const contentType = 'image/png'
-
 export async function GET() {
   return new ImageResponse(
     (
